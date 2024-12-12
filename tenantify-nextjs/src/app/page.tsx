7de3aa77d6ceb7739eb04b03621764dda20183c5
Winaya Zarkasih <winayaid@gmail.com>
2024-12-12 21:47:57 +0700

    fix: add hostname check logic again

--- tenantify-nextjs/src/app/page.tsx
+++ tenantify-nextjs/src/app/page.tsx
@@ -1,39 +1,43 @@
 "use client";
 
-import { useState } from 'react';
+import {
+  useEffect,
+  useState,
+} from 'react';
 
 import { useSession } from 'next-auth/react';
 
 import { LoginForm } from '@/components/login-form';
+import api from '@/services/api';
 
 export default function Home() {
   const session = useSession();
   const [logo, setLogo] = useState("");
 
-  // useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     try {
-  //       if (window.location.hostname.split(".").length > 1) {
-  //         api
-  //           .get(`/tenant/check/${window.location.hostname.split(".")[0]}`)
-  //           .then((res) => {
-  //             setLogo(res?.data?.logo);
-  //           })
-  //           .catch(() => {
-  //             if (session?.data?.user) {
-  //               window.location.href = `/user`;
-  //             } else {
-  //               window.location.href = `${
-  //                 window.location.hostname.split(".")[1]
-  //               }`;
-  //             }
-  //           });
-  //       }
-  //     } catch (error) {
-  //       window.location.href = `${window.location.hostname}`;
-  //     }
-  //   }
-  // }, [session]);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      try {
+        if (window.location.hostname.split(".").length > 1) {
+          api
+            .get(`/tenant/check/${window.location.hostname.split(".")[0]}`)
+            .then((res) => {
+              setLogo(res?.data?.logo);
+            })
+            .catch(() => {
+              if (session?.data?.user) {
+                window.location.href = `/user`;
+              } else {
+                window.location.href = `${
+                  window.location.hostname.split(".")[1]
+                }`;
+              }
+            });
+        }
+      } catch (error) {
+        window.location.href = `${window.location.hostname}`;
+      }
+    }
+  }, [session]);
   return (
     <div>
       <LoginForm logo={logo} />
